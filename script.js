695d9abaed218b6ef0535e3495c94a573b098785
WekaMbali <klyntonmainya@gmail.com>
2021-02-09 19:51:03 +0300

    form-functionality

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+let map, mapEvent;
 //geolocation api
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
@@ -23,20 +24,53 @@ if (navigator.geolocation) {
             // );
 
             const coords = [latitude, longitude];
-            const map = L.map("map").setView(coords, 13);
+            map = L.map("map").setView(coords, 13);
 
-            L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-                attribution:
-                    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            }).addTo(map);
+            L.tileLayer(
+                "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+                {
+                    attribution:
+                        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                },
+            ).addTo(map);
 
-            L.marker(coords)
-                .addTo(map)
-                .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-                .openPopup();
+            //Handling clicks on the map
+            map.on("click", function (mapE) {
+                mapEvent = mapE;
+                form.classList.remove("hidden");
+                inputDistance.focus();
+            });
         },
         function () {
             alert("Could not get your position");
         },
     );
 }
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    //clear input fields
+    inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value =
+        "";
+
+    //Display Marker
+    const { lat, lng } = mapEvent.latlng; //Destructuring the mapEvent
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+            L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: "running-popup",
+            }),
+        )
+        .setPopupContent("Workout")
+        .openPopup();
+});
+
+inputType.addEventListener("change", function () {
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+});
